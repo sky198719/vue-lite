2845ed0c3e1eca7fd2d486990ba3017cec2faf11
zhangjunjie <sh-zhangjj7@chinaunicom.cn>
2019-01-18 14:18:23 +0800

    test

--- src/assets/js/global.js
+++ src/assets/js/global.js
@@ -2,11 +2,21 @@ import axios from 'axios'
 import qs from 'qs'
 
 const getData = (type,url,data) => {
+	let headers = ''
+	let obj = ''
+	if(type == 'file'){
+		type = 'post'
+		obj = data
+		headers = {'Content-Type':'multipart/form-data'}
+	}else{
+		obj = qs.stringify(data)
+		headers = {'Content-Type':'application/x-www-form-urlencoded'}
+	}
 	return axios({
 		method:type,
 		url:url,
-		data:qs.stringify(data),
-		headers: {'Content-Type':'application/x-www-form-urlencoded'}
+		data:obj,
+		headers:headers
 	})
 	.then((res) => res.data)
 	.catch((err) => {
@@ -14,4 +24,6 @@ const getData = (type,url,data) => {
 	})
 }
 
-export {getData}
+const localPath = '/'
+
+export {getData,localPath}
