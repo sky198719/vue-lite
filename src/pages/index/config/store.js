15e0ba8959cbb1dd5678d16d8672df86a353e80f
zhangjunjie <sh-zhangjj7@chinaunicom.cn>
2019-05-24 10:02:40 +0800

    test

--- src/pages/index/config/store.js
+++ src/pages/index/config/store.js
@@ -1,12 +1,16 @@
-const store = {
-	state:{
-		title:''
-	},
-	mutations:{
-		setTitle(state,data){
-			state.title = data
-		}
+const state = {
+	title:''
+}
+
+const mutations = {
+	setTitle(state,data){
+		state.title = data
 	}
+}
+
+const store = {
+	state:state,
+	mutations:mutations
 }
 
 export default store
